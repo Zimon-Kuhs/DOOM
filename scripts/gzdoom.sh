b22cd6329b949a3c0d2169fae395ed1b283dcccc
Zimon Kuhs <zimonkuhs@gmail.com>
2023-05-18 18:50:45 +0200

    Script v2.

--- scripts/gzdoom.sh
+++ scripts/gzdoom.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+say () {
+    echo "[$(basename "$(dirname "${0%.*}"))]$1")"
+}
+
 error () {
     if [ -n "$1" ]; then
         echo "$1"
@@ -23,16 +27,41 @@ check () {
     fi
 }
 
+isExtension() {
+    file="(basename $1)"
+
+    for extension in ${*:2}; do
+        if [ "$(basename "$file" $extension)" = "$file" ]; then
+            echo true
+        fi
+    done
+    echo false
+}
+
+warning () {
+    say "[WARNING]: $1"
+}
+
+#~#~################################################################################################################~#~#
+#   Default values.
+#~#~################################################################################################################~#~#
+
 category="max"
 demo=""
-difficulty="4"
 files=""
-iwad="doom2"
+iwad="DOOM2"
 map="01"
-pistolStart="$GZDOOM_DIR/pistolstart.pk3"
-smoothDoom="$GZDOOM_DIR/SmoothDoom.pk3"
+skill="4"
 target=""
+tester=""
 verbose=false
+
+modFiles=()
+modFiles+=("SmoothDoom.pk3")
+
+#~#~################################################################################################################~#~#
+#   Git Bash friendly getopt.
+#~#~################################################################################################################~#~#
 
 POSITIONAL_ARGS=()
 while [[ $# -gt 0 ]]; do
@@ -41,27 +70,24 @@ while [[ $# -gt 0 ]]; do
     shifts=true
 
     case "$opt" in
-        -c) check "-c" "$param"
-            category="$param"
+        -c) category="$(check "$opt" "$param")"
             ;;
-        -d) check "-d" "$param"
-            demo="$param"
+        -d) demo="$(check "$opt" "$param")"
             ;;
-        -m) check "-m" "$param"
-            map="$param"
+        -m) map="$(check "$opt" "$param")"
             ;;
-        -s) check "-s" "$param"
-            difficulty="$param"
+        -s) difficulty="$(check "$opt" "$param")"
             ;;
-        -t) check "-t" "$param"
-            testFolder="_test/$param/"
+        -t) tester="$(check "$opt" "$param")"
             ;;
         v)  verbose=true
+            shifts=false
             ;;
         -*) error "No such option: $1"
             ;;
         *)  POSITIONAL_ARGS+=("$opt")
-            shifts=false;;
+            shifts=false
+            ;;
     esac
 
     shift
@@ -71,80 +97,123 @@ while [[ $# -gt 0 ]]; do
 done
 set -- "${POSITIONAL_ARGS[@]}"
 
+#~#~################################################################################################################~#~#
+#   Figure out the WAD dir and check if it exists.
+#~#~################################################################################################################~#~#
+
 if [ -z "$1" ]; then
     error "Need target."
 fi
-target="${1,,}"
 
-targetFolder="$testFolder$target"
-wadDir="$GZDOOM_DIR/wad/$targetFolder"
-if [ ! -d "$wadDir" ]; then
-    error "Could not find WAD dir: \"$wadDir\""
+testDir=""
+wadType="pwad"
+if [ -n "$tester" ]; then
+    testDir="$tester/"
+    wadType="twad"
 fi
+
+dirSuffix="$testDir${1,,}"
+wadDir="$wadType/$dirSuffix"
+if [ ! -d "$wadDir" ]; then
+    error "Could not find WAD dir: $wadDir"
+fi
+
+#~#~################################################################################################################~#~#
+#   Add extra files for the '-file' parameter.
+#~#~################################################################################################################~#~#
 
 files=()
 for file in "$wadDir"/*; do
-    if [ -f "$file" ]; then
-        if [ "$(basename "$file" .bat)" = "$(basename "$file")" ] && \
-           [ "$(basename "$file" .txt)" = "$(basename "$file")" ] && \
-           [ "$(basename "$file" .rar)" = "$(basename "$file")" ] && \
-           [ "$(basename "$file" .zip)" = "$(basename "$file")" ]; then
-
-            files+=("$file")
-        fi
+    if [ -f "$file" ] && \
+       [ "$(isExtension "$file" ".bat" ".rar" ".txt" ".zip")" = false ]; then
+        files+=("$file")
     fi
 done
 
+#~#~################################################################################################################~#~#
+#   Set IWAD and complevel.
+#   Also, any PWAD-specific considerations should be added here.
+#~#~################################################################################################################~#~#
+
 case "$target" in
-    "MM")
-        pistolStart=""
-        smoothDoom=""
+    "plutonia2")
+        iwad="PLUTONIA"
         ;;
     "tntr")
         iwad="TNT"
         ;;
     "valiant")
-        pistolStart=""
-        smoothDoom=""
+        modFiles=()
         ;;
 esac
 
 complevel=2
 case "$iwad" in
-    "TNT")  complevel=4
-            ;;
-    "Plutonia")
-            complevel=4
-            ;;
+    "DOOM")     complevel=9 ;;
+    "TNT")      complevel=4 ;;
+    "Plutonia") complevel=4 ;;
 esac
 
-for file in $smoothDoom $pistolStart; do
-    if [ -n "$file" ]; then
+#~#~################################################################################################################~#~#
+#   Check if mod files exist.
+#~#~################################################################################################################~#~#
+
+for file in "${modFiles[@]}"; do
+    if [ -z "$file" ]; then
+        continue
+    fi
+
+    modFile="$GZDOOM_MOD_DIR/$file"
+    if [ -f "$modFile" ]; then
         files+=("$file")
+    else
+        warning "$modFile not found."
     fi
 done
+
+#~#~################################################################################################################~#~#
+#   Calculate the name of the demo file.
+#~#~################################################################################################################~#~#
 
 if [ "${#map}" = 1 ]; then
     map="0$map"
 fi
 
-outputDir="$GZDOOM_DIR/demo/$targetFolder/map$map"
-demoPath="$outputDir/$DOOM_PLAYER-$target-$map-skill${difficulty}-${category}"
+difficulty="uv"
+case "$skill" in
+    1)  difficulty="itytd"  ;;
+    2)  difficulty="hntr"   ;;
+    3)  difficulty="hmp"    ;;
+    5)  difficulty="nm"     ;;
+esac
+
+demoDir="$DOOM_DEMO_DIR/$GZDOOM_LATEST_VERSION/$dirSuffix/map$map"
+demoName="$DOOM_PLAYER-$target-$map-$difficulty-${category}"
 
 if [ -n "$demo" ]; then
     demoCommand="-playdemo"
-    demoFile="${demoPath}_$demo.lmp"
+    demoNumber="$demo"
 else
-    mkdir -p "$outputDir"
     demoCommand="-record"
-    demoFile="${demoPath}_$(find "$outputDir" -maxdepth 1 -type f | wc -l).lmp"
+    demoNumber="$(find "$demoDir" -maxdepth 2 -type f -name "*.lmp" | wc -l)"
+
+    mkdir -p "$demoDir"
+fi
+demoFile="${demoName}_$demoNumber"
+
+if [ -n "$demo" ]; then
+    if [ ! -f "$demoFile" ]; then
+        error "No such demo file exists: $demoFile"
+    fi
+elif [ -f "$demoFile" ]; then
+    error "Critical error, $demoFile already exists somehow."
 fi
 
 if [ "$verbose" = true ]; then
     set -x
 fi
 
-"$GZDOOM_DIR/gzdoom.exe" -file ${files[*]} \
+"$GZDOOM_DIR/gzdoom.exe" -file "${files[@]}" \
                          -iwad "$iwad.wad" \
                          -complevel "$complevel" \
                          -skill "$difficulty" \
