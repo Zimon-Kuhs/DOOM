8701ea3f6d778d66b7805b66bb40a3dd19ad3200
Zimon-Kuhs <zimon.kuhs@axis.com>
2023-10-15 14:47:04 +0200

    Update.

--- src/port/gzdoom.py
+++ src/port/gzdoom.py
@@ -628,7 +628,7 @@ def readLaunch(argv):
                                                     help    = "Extra files; use for testing.",
                                                     nargs   = "*")
 
-    parser.add_argument("-g", "--configuration",    default = os.path.join(os.path.dirname(sys.argv[0]), "../pwads.json"),
+    parser.add_argument("-g", "--configuration",    default = os.path.join(os.path.dirname(sys.argv[0]), env("DOOM_SRC"), "data", "pwads.json"),
                                                     help    = "Configuration file to use.")
 
     parser.add_argument("-i", "--practice",         action  = "store_const",
